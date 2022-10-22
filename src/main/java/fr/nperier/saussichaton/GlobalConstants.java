fef8104244ef8e4050536fefdb7046064934dd33
Nathan PERIER <periernathan2000@gmail.com>
2022-10-22 21:41:37 +0200

    Added a class to hold the main constants for the game

---
+++ src/main/java/fr/nperier/saussichaton/GlobalConstants.java
@@ -0,0 +1,15 @@
+package fr.nperier.saussichaton;
+
+import fr.nperier.saussichaton.engine.GameState;
+
+public class GlobalConstants {
+
+    public static final int MIN_PLAYERS = 2;
+    public static final int MAX_PLAYERS = 5;
+
+    public static final GameState BEGIN_STATE = GameState.INIT;
+    public static final int BEGIN_CARD_COUNT = 7;
+
+    public static final String BASE_EXTENSION = "base";
+
+}
