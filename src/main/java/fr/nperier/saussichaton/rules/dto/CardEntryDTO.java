3522830b5fe0bc6e15e263d4be9919d8f296b4dc
Nathan PERIER <periernathan2000@gmail.com>
2022-10-22 21:23:59 +0200

    Added objects for reading the configuration and configuration for the cards (NOT TESTED)

---
+++ src/main/java/fr/nperier/saussichaton/rules/dto/CardEntryDTO.java
@@ -0,0 +1,45 @@
+package fr.nperier.saussichaton.rules.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import fr.nperier.saussichaton.GlobalConstants;
+import fr.nperier.saussichaton.engine.GameState;
+import fr.nperier.saussichaton.utils.MappingFunction;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Map;
+
+public class CardEntryDTO {
+
+    @Getter @Setter
+    private String name;
+    @Getter
+    private String extension = GlobalConstants.BASE_EXTENSION;
+    @JsonIgnore
+    private MappingFunction<Integer> numberFunction;
+    @JsonProperty("given_at_start")
+    @Getter @Setter
+    private int givenAtStart = 0;
+    @Getter @Setter
+    @JsonProperty("insert_state")
+    private GameState insertState;
+    @Getter @Setter
+    @JsonProperty("draw_action")
+    private String drawAction = null;
+
+    @JsonProperty("count_in_pile")
+    public void setNumberMapping(final Map<String, Integer> mapping) {
+        numberFunction = new MappingFunction<>(mapping);
+    }
+
+    public void setExtension(String extension) {
+        if(extension != null) {
+            this.extension = extension;
+        }
+    }
+
+    public int getInitialNumber(final int nPlayers) {
+        return numberFunction.apply(nPlayers);
+    }
+}
---
+++ src/main/java/fr/nperier/saussichaton/rules/dto/CardPlayDTO.java
@@ -0,0 +1,28 @@
+package fr.nperier.saussichaton.rules.dto;
+
+import fr.nperier.saussichaton.GlobalConstants;
+import fr.nperier.saussichaton.engine.GameState;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Map;
+
+@Getter
+public class CardPlayDTO {
+
+    private String extension = GlobalConstants.BASE_EXTENSION;
+    @Setter
+    private Map<String, Integer> cards;
+    @Setter
+    private List<GameState> states;
+    @Setter
+    private String action;
+
+    public void setExtension(String extension) {
+        if(extension != null) {
+            this.extension = extension;
+        }
+    }
+
+}
