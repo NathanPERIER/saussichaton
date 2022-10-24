9cd5b439bb3ef43ca7d5ffe1f21784b8913b11b6
Nathan PERIER <periernathan2000@gmail.com>
2022-10-24 13:36:51 +0200

    Added channel overlay for prompts

---
+++ src/main/java/fr/nperier/saussichaton/networking/helpers/ChannelPromptOverlay.java
@@ -0,0 +1,33 @@
+package fr.nperier.saussichaton.networking.helpers;
+
+import fr.nperier.saussichaton.engine.Player;
+import fr.nperier.saussichaton.networking.CommChannel;
+import fr.nperier.saussichaton.networking.prompt.ListPrompt;
+import fr.nperier.saussichaton.networking.prompt.ListResults;
+import fr.nperier.saussichaton.rules.data.Card;
+
+import java.util.List;
+
+public class ChannelPromptOverlay {
+
+    private final CommChannel channel;
+
+    public ChannelPromptOverlay(final CommChannel channel) {
+        this.channel = channel;
+    }
+
+    public ListResults<Card> promptCards(final String message, final Player player) {
+        ListPrompt<Card> prompt = ListPrompt.<Card>create(message)
+                .addAll(player.getHand())
+                .build();
+        return player.getCommunicator().multiChoice(prompt);
+    }
+
+    public ListResults<Card> promptCards(final String message, final Player player, final List<Boolean> canUse) {
+        ListPrompt<Card> prompt = ListPrompt.<Card>create(message)
+                .addAll(player.getHand(), canUse)
+                .build();
+        return player.getCommunicator().multiChoice(prompt);
+    }
+
+}
