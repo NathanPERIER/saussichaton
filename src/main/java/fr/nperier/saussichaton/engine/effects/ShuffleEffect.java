14c0e374c6b6b5f97ceec7598ed53cf1e3cbd8a3
Nathan PERIER <periernathan2000@gmail.com>
2022-10-25 16:04:46 +0200

    Added an effect to shuffle the deck

---
+++ src/main/java/fr/nperier/saussichaton/engine/effects/ShuffleEffect.java
@@ -0,0 +1,28 @@
+package fr.nperier.saussichaton.engine.effects;
+
+import fr.nperier.saussichaton.engine.CardEffect;
+import fr.nperier.saussichaton.engine.DrawPile;
+import fr.nperier.saussichaton.engine.GameState;
+import fr.nperier.saussichaton.engine.Player;
+import fr.nperier.saussichaton.networking.CommChannel;
+import fr.nperier.saussichaton.rules.CardEffectRegistry;
+
+@CardEffectRegistry.RegisterEffect("shuffle")
+public class ShuffleEffect extends CardEffect {
+
+    private final DrawPile drawPile;
+    private final CommChannel channel;
+
+    public ShuffleEffect(final Player currentPlayer, final DrawPile drawPile, final CommChannel channel) {
+        super(currentPlayer);
+        this.drawPile = drawPile;
+        this.channel = channel;
+    }
+
+    @Override
+    public GameState execute() {
+        drawPile.shuffle();
+        channel.broadcast("The draw pile has been thoroughly shuffled");
+        return GameState.TURN_CHOICE;
+    }
+}
