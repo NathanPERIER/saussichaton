92b9763bb8d5f3274e5ba9ca216ffddb022e8f10
Nathan PERIER <periernathan2000@gmail.com>
2022-10-25 14:56:02 +0200

    Added annotations for targeted effects and interactive effects

--- src/main/java/fr/nperier/saussichaton/engine/CardEffect.java
+++ src/main/java/fr/nperier/saussichaton/engine/CardEffect.java
@@ -2,8 +2,17 @@ package fr.nperier.saussichaton.engine;
 
 import fr.nperier.saussichaton.injection.Resolvable;
 import lombok.Getter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 public abstract class CardEffect implements Resolvable {
+
+    private static final Logger logger = LogManager.getLogger(CardEffect.class);
 
     @Getter
     protected final Player player;
@@ -12,6 +21,28 @@ public abstract class CardEffect implements Resolvable {
         this.player = player;
     }
 
+    public boolean target() {
+        logger.warn("Default implementation of bool target() called");
+        return true;
+    }
+
     public abstract GameState execute();
+
+    public boolean isTargeted() {
+        return this.getClass().isAnnotationPresent(Targeted.class);
+    }
+
+    public boolean isInteractive() {
+        return this.getClass().isAnnotationPresent(Interactive.class);
+    }
+
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target({ElementType.TYPE})
+    public @interface Targeted { }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target({ElementType.TYPE})
+    public @interface Interactive { }
 
 }
