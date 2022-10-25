0f6efe76e890a9ccbd628300554ff81f0f2b34c5
Nathan PERIER <periernathan2000@gmail.com>
2022-10-25 15:07:39 +0200

    Updated the effect for the exploding kitten

--- src/main/java/fr/nperier/saussichaton/engine/effects/PrimeExplosionEffect.java
+++ src/main/java/fr/nperier/saussichaton/engine/effects/PrimeExplosionEffect.java
@@ -3,7 +3,9 @@ package fr.nperier.saussichaton.engine.effects;
 import fr.nperier.saussichaton.engine.CardEffect;
 import fr.nperier.saussichaton.engine.GameState;
 import fr.nperier.saussichaton.engine.Player;
+import fr.nperier.saussichaton.rules.CardEffectRegistry;
 
+@CardEffectRegistry.RegisterEffect("prime_explosion")
 public class PrimeExplosionEffect extends CardEffect {
 
     public PrimeExplosionEffect(final Player currentPlayer) {
