758b0698b633625ee53e5a0a4c351bc23c8a5b8f
Nathan PERIER <periernathan2000@gmail.com>
2022-10-25 14:56:21 +0200

    Improved annotations

--- src/main/java/fr/nperier/saussichaton/injection/annotations/AutoResolve.java
+++ src/main/java/fr/nperier/saussichaton/injection/annotations/AutoResolve.java
@@ -1,8 +1,11 @@
 package fr.nperier.saussichaton.injection.annotations;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.CONSTRUCTOR})
 public @interface AutoResolve {
 }
