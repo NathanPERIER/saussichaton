59249d35ebe104d85623a6aca5f8d60e6325b0c8
Nathan PERIER <periernathan2000@gmail.com>
2022-10-26 19:46:02 +0200

    Used the new helper to get the cards from the player

--- src/main/java/fr/nperier/saussichaton/engine/states/explosion/PrimeExplosionState.java
+++ src/main/java/fr/nperier/saussichaton/engine/states/explosion/PrimeExplosionState.java
@@ -5,7 +5,9 @@ import fr.nperier.saussichaton.engine.GameEngine;
 import fr.nperier.saussichaton.engine.GameState;
 import fr.nperier.saussichaton.engine.Player;
 import fr.nperier.saussichaton.engine.StateAction;
-import fr.nperier.saussichaton.networking.helpers.ChannelPromptOverlay;
+import fr.nperier.saussichaton.networking.CommChannel;
+import fr.nperier.saussichaton.networking.helpers.CardPlayResult;
+import fr.nperier.saussichaton.networking.helpers.Prompts;
 import fr.nperier.saussichaton.networking.prompt.ListResults;
 import fr.nperier.saussichaton.rules.CardPlayTree;
 import fr.nperier.saussichaton.rules.data.Card;
@@ -17,41 +19,34 @@ import java.util.Optional;
 public class PrimeExplosionState extends StateAction {
 
     private final Player currentPlayer;
-    private final ChannelPromptOverlay prompts;
     private final CardPlayTree cardPlays;
     private final GameState currentState;
     private final GameEngine engine;
 
-    public PrimeExplosionState(final Player currentPlayer, final ChannelPromptOverlay prompts,
-                               final CardPlayTree cardPlays, final GameState currentState,
+    public PrimeExplosionState(final Player currentPlayer, final CardPlayTree cardPlays, final GameState currentState,
                                final GameEngine engine) {
         this.currentPlayer = currentPlayer;
-        this.prompts = prompts;
         this.cardPlays = cardPlays;
         this.currentState = currentState;
         this.engine = engine;
     }
 
+    private CardPlayResult prompt() {
+        return Prompts.promptCardPlay(
+                "You are going to explode, what do you want to do ?",
+                currentPlayer, currentState, cardPlays, engine,
+                null
+        );
+    }
+
     @Override
     public GameState execute() {
-        List<Boolean> canUse = cardPlays.canPlay(currentPlayer.getHand(), currentState);
-        if(canUse.stream().anyMatch(b -> b)) {
-            while(true) {
-                ListResults<Card> res = prompts.promptCards(
-                        "You are going to explode, what do you want to do ?",
-                        currentPlayer, canUse
-                );
-                Optional<CardPlay> cardPlay = cardPlays.get(res.getValues());
-                if(cardPlay.isPresent()) {
-                    currentPlayer.removeCards(res.getIndexes());
-                    Optional<CardEffect> effect = engine.initEffect(cardPlay.get().getAction());
-                    if(effect.isPresent()) {
-                        return effect.get().execute().orElse(GameState.TURN_END);
-                    }
-                }
-            }
+        CardPlayResult res = prompt();
+        if(res.isImpossible()) {
+            return GameState.EXPLODE;
         }
-        return GameState.EXPLODE;
+        currentPlayer.removeCards(res.getIndexes());
+        return res.getEffect().execute().orElse(GameState.TURN_END);
     }
 
 }
