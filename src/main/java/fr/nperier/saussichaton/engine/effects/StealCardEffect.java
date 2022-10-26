171004038204705c54c57689562b188de13ea96d
Nathan PERIER <periernathan2000@gmail.com>
2022-10-26 17:41:55 +0200

    Changed the name for a targeted card effect

--- src/main/java/fr/nperier/saussichaton/engine/effects/StealCardEffect.java
+++ src/main/java/fr/nperier/saussichaton/engine/effects/StealCardEffect.java
@@ -4,6 +4,9 @@ import fr.nperier.saussichaton.engine.CardEffect;
 import fr.nperier.saussichaton.engine.Player;
 import fr.nperier.saussichaton.networking.prompt.ListPrompt;
 import fr.nperier.saussichaton.networking.prompt.ListResult;
+import fr.nperier.saussichaton.rules.data.Card;
+
+import java.util.Map;
 
 public abstract class StealCardEffect extends CardEffect {
 
@@ -11,6 +14,11 @@ public abstract class StealCardEffect extends CardEffect {
 
     public StealCardEffect(final Player currentPlayer) {
         super(currentPlayer);
+    }
+
+    @Override
+    public String getName(final Map<Card,Integer> cards) {
+        return super.getName(cards) + " on " + target;
     }
 
     @Override
