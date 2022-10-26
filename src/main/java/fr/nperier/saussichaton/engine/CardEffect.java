7a89d75084fe8920de341698625c6ad17825b4f6
Nathan PERIER <periernathan2000@gmail.com>
2022-10-26 17:41:21 +0200

    Added a name to the card effects

--- src/main/java/fr/nperier/saussichaton/engine/CardEffect.java
+++ src/main/java/fr/nperier/saussichaton/engine/CardEffect.java
@@ -1,6 +1,7 @@
 package fr.nperier.saussichaton.engine;
 
 import fr.nperier.saussichaton.injection.Resolvable;
+import fr.nperier.saussichaton.rules.data.Card;
 import lombok.Getter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -9,7 +10,11 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public abstract class CardEffect implements Resolvable {
 
@@ -37,8 +42,16 @@ public abstract class CardEffect implements Resolvable {
         return this.getClass().isAnnotationPresent(Targeted.class);
     }
 
-    public boolean isInteractive() {
-        return this.getClass().isAnnotationPresent(Interactive.class);
+    protected String constructName(final Map<Card, Integer> cards, final Function<Card,String> namer) {
+        final List<String> cards_repr = cards.entrySet().stream()
+                .sorted((e1, e2) -> e1.getKey().compareTo(e2.getKey()))
+                .map(e -> namer.apply(e.getKey()) + (e.getValue() > 1 ? " x" + e.getValue() : ""))
+                .collect(Collectors.toList());
+        return String.join(" + ", cards_repr);
+    }
+
+    public String getName(final Map<Card, Integer> cards) {
+        return constructName(cards, Card::toString);
     }
 
 
